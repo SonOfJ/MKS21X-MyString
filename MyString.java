aa3c5d4d8b83e8804e03efdba541cba7a1ea033d
Jason Zheng <zzheng3@stuy.edu>
2018-11-21 11:35:31 -0500

    finished with constructor

--- MyString.java
+++ MyString.java
@@ -1,6 +1,9 @@
 public class MyString implements CharSequence,Comparable<CharSequence>{
   private char[] data;
   public MyString(CharSequence s){
-
+    data = new char[CharSequence.length];
+    for(int i = 0; i < data.length; i = i + 1) {
+      data[i] = CharSequence.charAt(i);
+    }
   }
 }
