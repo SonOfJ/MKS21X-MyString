afa3e1b10c8ecd34dcb0448f03d142fcbce05c18
Jason Zheng <zzheng3@stuy.edu>
2018-11-25 01:32:24 -0500

    added comments

--- MyString.java
+++ MyString.java
@@ -1,18 +1,18 @@
 public class MyString implements CharSequence,Comparable<CharSequence>{
   private char[] data;
   public MyString(CharSequence s){
-    data = new char[CharSequence.length];
+    data = new char[CharSequence.length]; //Initiates an array with same length as CharSequence
     for(int i = 0; i < data.length; i = i + 1) {
-      data[i] = CharSequence.charAt(i);
+      data[i] = CharSequence.charAt(i); //Fills in the elements of CharSequence
     }
   }
   public char charAt(int index) {
-    return data[index];
+    return data[index]; //Returns the element at the index
   }
   public int length() {
     int length = 0;
     for(int i = 0; i < data.length; i = i + 1) {
-      length = length + 1;
+      length = length + 1; //Adds 1 for every element
     }
   }
   public CharSequence subSequence(int start, int end) {
